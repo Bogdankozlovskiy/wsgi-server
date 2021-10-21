e2004695c74fc8334578492399ccf7386ebb70be
bogdankozlovsky <bogdan.k@datarails.com>
2021-10-21 19:22:00 +0200

    add new end point

--- short_cuts.py
+++ short_cuts.py
@@ -2,8 +2,6 @@ def parse_body(body):
     data = {}
     if body is None:
         return data
-
-
     for input_ in body.decode().split("&"):
         key, value = input_.split("=")
         data[key] = value
@@ -14,8 +12,6 @@ def parse_cookies(http_cookie):
     data = {}
     if http_cookie is None:
         return data
-
-
     for input_ in http_cookie.split("; "):
         key, *value = input_.split("=")
         value = "=".join(value)
